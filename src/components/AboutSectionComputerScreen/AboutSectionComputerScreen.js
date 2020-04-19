adf94ff233452e64117a0540b3bb50fd59baed5e
fernandocgomez <fernandocgomez@live.com>
2020-04-19 17:29:42 -0500

    AboutSectionComputerScreen is done

--- src/components/AboutSectionComputerScreen/AboutSectionComputerScreen.js
+++ src/components/AboutSectionComputerScreen/AboutSectionComputerScreen.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import './AboutSectionComputerScreen.css';
-import { Responsive} from "semantic-ui-react";
-import profilePicture from '../../assets/profile-picture.jpg'
+import React from "react";
+import "./AboutSectionComputerScreen.css";
+import { Responsive } from "semantic-ui-react";
+import profilePicture from "../../assets/profile-picture.jpg";
 
 function AboutSectionComputerScreen() {
   return (
     <Responsive {...Responsive.onlyComputer}>
       <div className="computer-about-section-container">
         <div className="computer-about-section-img">
-          <img src={profilePicture} alt="Profile Picture"/>
+          <img src={profilePicture} alt="Profile Picture" />
         </div>
         <div className="computer-about-section-name">
-          <h2>
-            About me
-          </h2>
+          <h2>About me</h2>
         </div>
         <div className="computer-about-section-bio">
-          <p>Full-stack web developer with a passion for problem-solving and beautiful web design.</p>
-          <p>In the past 2+ years, I helped manage over 4 E-commerce websites, worked on a team of <br />
-          3+ developers, to build 4 full-stack web applications, and worked on 5 solo web and mobile <br /> app projects.</p>
+          <p>
+            Full-stack web developer with a passion for problem-solving and
+            beautiful web design.
+            In the past 2+ years, I helped manage over 4 E-commerce websites,
+            worked on a team of 3+ developers, to build 4 full-stack web
+            applications, and worked on 5 solo web and mobile app projects.
+          </p>
         </div>
       </div>
     </Responsive>
-  )
+  );
 }
 
-export default AboutSectionComputerScreen
+export default AboutSectionComputerScreen;
